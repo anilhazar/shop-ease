71d15b5e8e7430f617649377be6965791b28cea5
anilhazar <alianilhazar@gmail.com>
2024-07-03 17:30:37 +0300

    Add DTO(Request) ChangePasswordRequest and LoginRequest Classes of Authentication Layer to Handle User Input

---
+++ src/main/java/com/shopease/shopease/authentication/dto/ChangePasswordRequest.java
@@ -0,0 +1,26 @@
+package com.shopease.shopease.authentication.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChangePasswordRequest {
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String oldPassword;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String newPassword;
+
+}
